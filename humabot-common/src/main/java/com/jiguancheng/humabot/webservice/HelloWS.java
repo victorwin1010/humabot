b026173fcb279c82e9a62d5efdd9745d6859d7ed
juforg@gmail.com <juforg@gmail.com@dadebd25-c611-c628-1e7b-9faaf25b7e52>
2012-09-29 04:41:02 +0000

    webservice with soap

--- humabot-common/src/main/java/com/jiguancheng/humabot/webservice/HelloWS.java
+++ humabot-common/src/main/java/com/jiguancheng/humabot/webservice/HelloWS.java
@@ -1,8 +1,18 @@
 package com.jiguancheng.humabot.webservice;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+import com.jiguancheng.humabot.entity.User;
 
 @WebService
+@SOAPBinding(style = Style.RPC)
 public interface HelloWS{
 	public String greeting(String userName);
+	
+	public User getUserByName(@WebParam(name = "name") String name);
+	
+	public void setUser(User user);
 }
