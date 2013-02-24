3ef837d83a1a7b9256dcbfa47a79c97882084179
juforg@gmail.com <juforg@gmail.com@dadebd25-c611-c628-1e7b-9faaf25b7e52>
2013-02-24 12:42:29 +0000

    能够运行

--- humabot-common/src/main/java/com/jiguancheng/humabot/service/login/LoginServiceImpl.java
+++ humabot-common/src/main/java/com/jiguancheng/humabot/service/login/LoginServiceImpl.java
@@ -2,21 +2,23 @@ package com.jiguancheng.humabot.service.login;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
 
-import com.jiguancheng.humabot.dao.UserDAO;
+import org.apache.log4j.Logger;
+
+import com.jiguancheng.humabot.dao.UserDao;
 import com.jiguancheng.humabot.entity.UserInfo;
 
 public class LoginServiceImpl implements LoginService {
 	static Logger logger = Logger.getLogger(LoginServiceImpl.class);
-	@Autowired
-	public UserDAO userInfoDAO;
+	
+	@Resource(name="userDao")//注解方式注入
+	public UserDao userInfoDao;
 	
 	@Override
 	public boolean doLogin(String username, String password) {
 		if (username != null) {
-			UserInfo user = userInfoDAO.getUserByName(username);
+			UserInfo user = userInfoDao.getUserByName(username);
 			if (user!=null && user.getPassword().equals(password)) {
 				return true;
 			}
@@ -26,7 +28,7 @@ public class LoginServiceImpl implements LoginService {
 	
 	@Override
 	public boolean checkName(String username) {
-		List<String> list = userInfoDAO.getAllUserName();
+		List<String> list = userInfoDao.getAllUserName();
 		for (String name : list) {
 			if (name.equals(username)) {
 				return true;
--- humabot-common/src/test/java/com/jiguancheng/humabot/LoginTest.java
+++ humabot-common/src/test/java/com/jiguancheng/humabot/LoginTest.java
@@ -1,19 +1,23 @@
 package com.jiguancheng.humabot;
 
+import javax.annotation.Resource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+
+import com.jiguancheng.humabot.service.login.LoginService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring-test.xml")
 @TransactionConfiguration
 public class LoginTest {
-/*	@Autowired
-	private LoginService loginSer;*/
+	@Resource(name="loginService")
+	private LoginService loginSer;
 	
 	@Test
 	public void testLogin(){
-//		loginSer.doLogin("username", "password1");
+		loginSer.doLogin("username", "password1");
 	}
 }
