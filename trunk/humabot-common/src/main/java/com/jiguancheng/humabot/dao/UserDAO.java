3881d06bea7e604b01d20722f0f4260c85c78333
juforg@gmail.com <juforg@gmail.com@dadebd25-c611-c628-1e7b-9faaf25b7e52>
2013-02-24 13:51:24 +0000

    能够运行

--- trunk/humabot-common/src/main/java/com/jiguancheng/humabot/dao/UserDAO.java
+++ trunk/humabot-common/src/main/java/com/jiguancheng/humabot/dao/UserDAO.java
@@ -3,7 +3,7 @@ package com.jiguancheng.humabot.dao;
 import java.util.List;
 import com.jiguancheng.humabot.entity.UserInfo;
 
-public interface UserDao {
+public interface UserDAO {
 	public UserInfo getUserByName(String name);
 	public List<String> getAllUserName();
 }
